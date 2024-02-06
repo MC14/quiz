5d9e07a9ec70d678fe0c39abc03bde464c99b306
Morgan Cowans <cowansm@sheridancollege.ca>
2024-02-06 10:24:19 -0500

    destroyed class, added all methods to controller class, still gets error 500 parsing error

--- src/main/java/ca/sheridancollege/mcowans/quiz1/controllers/Controller.java
+++ src/main/java/ca/sheridancollege/mcowans/quiz1/controllers/Controller.java
@@ -1,24 +1,58 @@
 package ca.sheridancollege.mcowans.quiz1.controllers;
 
 
-import ca.sheridancollege.mcowans.quiz1.beans.Tablenum;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Controller {
 
-    ArrayList<Integer> Tableamount = new ArrayList<Integer>();
     Random rand = new Random();
-    @GetMapping("/")
-    public String index()
+
+    int amount = rand.nextInt(1,9);
+
+    ArrayList<Integer> StartTable = new ArrayList<>();
+
+    void addnewTable(ArrayList<Integer> Table, int amount)
     {
-        int amount = rand.nextInt(1,9);
+        int i = 0;
+        while ( i < amount)
+        {
+
+            int num = rand.nextInt(1,9);
+            Table.add(num);
+            i++;
+        }
+    }
+
+    public List<Integer> getStart()
+    {
+        return StartTable;
+    }
+    //Allows you to get the Table in order to pass it through as an attribute to be used in the table html
 
 
-
-
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        addnewTable(StartTable, amount);
+        model.addAttribute("tablenum", getStart());
        return "index";
     }
+
+    @PostMapping("/result")
+    public String result(Model model, @ModelAttribute int choice)
+    {
+        for (int i : StartTable){
+            i = i * choice;
+        }
+        model.addAttribute("tablenum", getStart());
+        return "result";
+    }
+    //Gets the choice from the select in html and uses it to multiply each
 }
